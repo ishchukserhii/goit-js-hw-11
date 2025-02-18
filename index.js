f3473a7d6d6a13328a512c86601aeeb9a405699f
ishchukserhii <ischuksergei@gmail.com>
2025-02-18 15:21:21 +0000

    Deploying to gh-pages from @ ishchukserhii/goit-js-hw-11@d114ec386ad3ac45eb9168b2f4fe03bb934fb378 🚀

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+import{a as u,i,S as f}from"./assets/vendor-D0cagnvz.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))a(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&a(n)}).observe(document,{childList:!0,subtree:!0});function s(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerPolicy&&(r.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?r.credentials="include":t.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(t){if(t.ep)return;t.ep=!0;const r=s(t);fetch(t.href,r)}})();const p="https://pixabay.com",d="/api";function m(e){const o=new URLSearchParams({key:"48878301-11eef390b295f332c6628756b",q:e,image_type:"photo",orientation:"horizontal",safesearch:"true"}),s=p+d+`?${o}`;return u.get(s)}document.querySelector(".gallery");function g(e){return`<li>
+  <a class="gallery-link" href="${e.largeImageURL}">
+  <div class="card">
+  <img class="card-img" src="${e.webformatURL}" alt="${e.tags}"/>
+  <div class="card-info">
+  <div class="info-container">
+    <div class="info-labels">
+      <span>Likes</span>
+      <span>Views</span>
+      <span>Comments</span>
+      <span>Downloads</span>
+    </div>
+    <div class="info-values">
+      <span>${e.likes}</span>
+      <span>${e.views}</span>
+      <span>${e.comments}</span>
+      <span>${e.downloads}</span>
+    </div>
+  </div>
+</div>
+</a>
+</li>`}function l(e){return e.map(g).join("")}const c={btn:document.querySelector(".btn-search"),form:document.querySelector(".search-form"),msg:document.querySelector(".msg"),gallery:document.querySelector(".gallery")};c.form.addEventListener("submit",e=>{e.preventDefault();let o=e.target.elements.bookSearch.value.trim();if(o===""){i.show({backgroundColor:"rgba(255, 67, 67, 0.68)",messageColor:"rgba(255, 255, 255, 1)",close:"true",position:"topRight",title:"🚫",titleColor:"#fff",titleSize:"16px",message:"Поле пошуку порожнє"});return}else m(o).then(s=>{if(s.data.hits.length===0)i.show({backgroundColor:"rgba(255, 67, 67, 0.68)",messageColor:"rgba(255, 255, 255, 1)",close:"true",position:"topRight",title:"🚫",titleColor:"#fff",titleSize:"16px",message:"Sorry, there are no images matching your search query. Please try again!"});else{l(s.data.hits);const a=l(s.data.hits);c.gallery.innerHTML=a}})});new f(".gallery-link",{captionsData:"alt",captionPosition:"bottom",captionDelay:250});
 //# sourceMappingURL=index.js.map
